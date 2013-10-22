b4d6f0c50d3e0df07ba2ef5d2da4a19df3e31095
Fuji, Goro <goro-fuji@cookpad.com>
2013-10-23 00:26:00 +0900

    add benchmark script

---
+++ tool/benchmark.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'benchmark'
+require 'visual_width'
+require 'unicode/display_width'
+
+str = "foo あいうえお bar αβγδε baz"
+
+VisualWidth.count(str) == str.display_width(2)
+
+[str, str * 5, str * 10].each do |str|
+  puts "\n", "for length #{str.length}:"
+  Benchmark.bmbm do |x|
+    x.report("visual_width") do
+      1000.times { VisualWidth.count(str) }
+    end
+    x.report("display_width") do
+      1000.times { str.display_width(2) }
+    end
+  end
+end
