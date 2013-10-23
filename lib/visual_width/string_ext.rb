6d931552fa841a74fa9aaf61a293d35116380f36
Fuji, Goro <goro-fuji@cookpad.com>
2013-10-24 00:07:01 +0900

    fix names

--- lib/visual_width/string_ext.rb
+++ lib/visual_width/string_ext.rb
@@ -1,11 +1,11 @@
 require 'visual_width'
 
 class String
-  def visual_width(opts = {})
+  def width(opts = {})
     VisualWidth.count(self, opts)
   end
 
-  def visual_truncate(max_length, opts = {})
+  def truncate(max_length, opts = {})
     VisualWidth.truncate(self, max_length, opts)
   end
 end
