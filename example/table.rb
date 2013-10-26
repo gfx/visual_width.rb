518c69dbf68880c7b8a4851d34e9c31dcd1b9a3d
Fuji, Goro <goro-fuji@cookpad.com>
2013-10-26 17:36:58 +0900

    update example

--- example/table.rb
+++ example/table.rb
@@ -2,16 +2,17 @@
 require 'visual_width/table'
 t = VisualWidth::Table.new(
   style: [
-    { align: :center, width:  8 },
-    { align: :center, width:  8 },
-    { align: :right,  width:  5 },
+    { align: :center, width:  10 },
+    { align: :center, width:  14 },
+    { align: :left,   width:  28 },
   ],
-  header: ['Nick', 'FullName', 'Age'],
+  header: ['Title', 'Author', 'Beginning'],
 )
 
+# cf. http://www.benricho.org/famous_novel/
 rows = [
-  ['カネダ', '金田 正太郎', 17],
-  ['テツオ', '島 鉄雄', 16],
-  ['ケイ', '?', 18],
+  ['蜘蛛の糸', '芥川 龍之介', 'ある日の事でございます。 御釈迦様は極楽の 蓮池のふちを、独りでぶらぶら御歩きになっていらっしゃいました。'],
+  ['こころ', '夏目 漱石', '私はその人を常に先生と呼んでいた。だからここでもただ先生と書くだけで本名は打ち明けない。'],
+  ['人間失格', '太宰 治', '私は、その男の写真を三葉、見たことがある。']
 ]
 puts t.render(rows)
