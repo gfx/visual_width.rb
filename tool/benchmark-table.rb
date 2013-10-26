df4be62b9a24089d12e0e3952efa77e5e25915f4
Fuji, Goro <goro-fuji@cookpad.com>
2013-10-26 15:38:44 +0900

    benchmark

---
+++ tool/benchmark-table.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'benchmark'
+require 'visual_width/table'
+require 'terminal-table'
+require 'text-table'
+
+header = [:Name, :Arity?, :DefinedInTheClass?]
+
+rows = String.instance_methods.map do |sym|
+  [sym, "".method(sym).arity, "".class.superclass.instance_methods.include?(sym)]
+end
+
+tabler = VisualWidth::Table.new(
+  header: header,
+  style: [
+    {align: :left},
+    {align: :right},
+    {align: :center},
+  ]
+)
+#puts tabler.render(rows)
+#puts Terminal::Table.new(rows: rows, headings: header)
+puts Text::Table.new(head: header, rows: rows)
+
+Benchmark.bmbm do |x|
+  x.report("visual_width/table") do
+    100.times { tabler.render(rows) }
+  end
+  x.report("terminal-table") do
+    100.times { Terminal::Table.new(rows: rows, headings: header).to_s }
+  end
+  x.report("text-table") do
+    100.times { Text::Table.new(rows: rows, head: header).to_s }
+  end
+end
